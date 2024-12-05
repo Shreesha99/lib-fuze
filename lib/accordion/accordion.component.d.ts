0b7d33c23dc009f89313716acd20dfd6f8b0e785
Shreesha99 <shreeshavenkatram99@gmail.com>
2024-12-05 07:51:43 +0530

    update: major changes to accordion

--- lib/accordion/accordion.component.d.ts
+++ lib/accordion/accordion.component.d.ts
@@ -1,9 +1,27 @@
-import { AfterContentInit } from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core';
 import * as i0 from "@angular/core";
-export declare class AccordionComponent implements AfterContentInit {
-    accordionItem: any;
-    constructor();
-    ngAfterContentInit(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<AccordionComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<AccordionComponent, "lib-accordion", never, {}, {}, ["accordionItem"], ["*"], true, never>;
+interface AccordionItem {
+    title: string;
+    content: string;
+    isOpen: boolean;
 }
+export declare class AccordionComponent implements OnChanges {
+    itemCount: number;
+    backgroundColor: string;
+    accordionBorderRadius: string;
+    itemBorderRadius: string;
+    width: string;
+    accordionHeight: string;
+    itemHeight: string;
+    itemGap: string;
+    isOpen: boolean;
+    accordionItems: AccordionItem[];
+    ngOnChanges(changes: SimpleChanges): void;
+    /** Generate accordion items dynamically */
+    private generateAccordionItems;
+    /** Toggle accordion item open/close */
+    toggle(item: AccordionItem): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<AccordionComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<AccordionComponent, "lib-accordion", never, { "itemCount": { "alias": "itemCount"; "required": false; }; "backgroundColor": { "alias": "backgroundColor"; "required": false; }; "accordionBorderRadius": { "alias": "accordionBorderRadius"; "required": false; }; "itemBorderRadius": { "alias": "itemBorderRadius"; "required": false; }; "width": { "alias": "width"; "required": false; }; "accordionHeight": { "alias": "accordionHeight"; "required": false; }; "itemHeight": { "alias": "itemHeight"; "required": false; }; "itemGap": { "alias": "itemGap"; "required": false; }; "isOpen": { "alias": "isOpen"; "required": false; }; }, {}, never, never, true, never>;
+}
+export {};
--- public-api.d.ts
+++ public-api.d.ts
@@ -2,7 +2,6 @@ export * from './lib/fuze-lib.service';
 export * from './lib/fuze-lib.component';
 export * from './lib/button/button.component';
 export * from './lib/accordion/accordion.component';
-export * from './lib/accordion/accordion-item.component';
 export * from './lib/avatar/avatar.component';
 export * from './lib/breadcrumb/breadcrumb.component';
 export * from './lib/dropdown-list/dropdown-list.component';
